fab0f7ef8b45f2fe96a33f01907cab9339d62b50
Arin Paliwal <paliwalarin09@gmail.com>
2024-01-02 23:03:16 +0530

    🚀 Initialized project

---
+++ core/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class UploadedFile(models.Model):
+    file = models.FileField()
+    uploaded_on = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.uploaded_on.date()
---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import FileUploadAPIView
+
+app_name = 'api'
+
+urlpatterns = [
+    path('upload-file/', FileUploadAPIView.as_view(), name='upload-file'),
+]
